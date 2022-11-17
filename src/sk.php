076f6f998d0c225b969dd2a0f1ed2e90343bbb32
Terka <tete.98765@gmail.com>
2022-11-17 20:26:45 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ src/sk.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+while ($a > 0) {
+	write("Faktorial nelze spocitat\n");
+}
+
--- src/tests/fact_non_rec.php
+++ src/tests/fact_non_rec.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 write("Zadejte cislo pro vypocet faktorialu\n");
 $a = readi();
 if ($a === null) {
-write("Chyba pri nacitani celeho cisla!\n");
-} else {}
+    write("Chyba pri nacitani celeho cisla!\n");
+} 
+else {}
 if ($a < 0) {
-write("Faktorial nelze spocitat\n");
-} else {
-$vysl = 1;
-while ($a > 0) {
-$vysl = $vysl * $a;
-$a = $a - 1;
+    write("Faktorial nelze spocitat\n");
+} 
+else {
+    $vysl = 1;
+    while ($a > 0) {
+    $vysl = $vysl * $a;
+    $a = $a - 1;
+    }
+    write("Vysledek je: ", $vysl, "\n");
 }
-write("Vysledek je: ", $vysl, "\n");
