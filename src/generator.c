52315aec6e4d0e093f29eab5402842635f9b6728
Marek Špirka <marek.spirka17@gmail.com>
2022-12-05 14:51:41 +0100

    Minor changes

--- src/generator.c
+++ src/generator.c
@@ -346,6 +346,7 @@ void gen_main(ht_table_t *table, int key){
 
     PRINT_LANE_ONE_ARG("LABEL", "$main");
     PRINT_LANE_ZERO_ARG("CREATEFRAME");
+    PRINT_LANE_ZERO_ARG("PUSHFRAME");
     for (int i = 0; i < key ; ++i) {
         element* e = NULL;
         sprintf(index, "%d", i);
