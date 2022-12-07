beb1b171cc9b60f974d1d374013625529ba5e25a
Marek Špirka <marek.spirka17@gmail.com>
2022-12-07 20:09:31 +0100

    Added comments

--- src/symtable.c
+++ src/symtable.c
@@ -1,6 +1,5 @@
 /*
  * Created by Marek Spirka and Jasmina Csalova on 17.11.2022
- *
  * Inspired by the second project from the IAL 2022 course
  */
 
@@ -11,29 +10,28 @@
 int HT_SIZE = MAX_HT_SIZE;
 
 int get_hash(char *key) {
-
     unsigned result;
 
     for ( result = 0; *key!= '\0'; key++)
         result = *key + 31 *  result;
     return  result % MAX_HT_SIZE;
 }
+
 void ht_init(ht_table_t *table) {
     for (int i = 0; i < MAX_HT_SIZE; i++){
         (*table)[i] = NULL;
     }
 }
+
 ht_item_t *ht_search(ht_table_t *table, char *key) {
     if (table == NULL) {
         return NULL;
     }
-    //index do tabulky
+
     int index = get_hash(key);
     ht_item_t *search = (*table)[index];
 
-    //dokym neprejdeme vsetko
     while(search != NULL){
-        //ak sa rovnaju
         if(search->key == *key){
             return search;
         }else{
@@ -42,11 +40,11 @@ ht_item_t *ht_search(ht_table_t *table, char *key) {
     }
     return NULL;
 }
+
 void ht_insert(ht_table_t *table, char *key, element *data) {
     if (table == NULL) {
         return;
     }
-
     ht_item_t *item_exist = ht_search(table, key);
 
     if(item_exist!= NULL){
@@ -54,38 +52,39 @@ void ht_insert(ht_table_t *table, char *key, element *data) {
     }
     else{
         int index = get_hash(key);
-        //alokujeme si pamat
+
         ht_item_t *item = malloc(sizeof (ht_item_t));
         if(item == NULL){
             callError(ERR_INTERNAL);
         }
-        //priradime hodnoty do noveho pridaneho prvku
         memcpy(&item->e, data, sizeof(element));
         memcpy(&item->key, key, sizeof(*key));
         (*table)[index] = item;
     }
 }
+
 element *ht_get(ht_table_t *table, char *key) {
-  ht_item_t *item = ht_search(table, key);
+    ht_item_t *item = ht_search(table, key);
 
-  if(item != NULL){
-    return &item->e;
-  } else {
-      return NULL;
-  }
+    if(item != NULL){
+        return &item->e;
+    } else {
+        return NULL;
+    }
 }
-void ht_delete_all(ht_table_t *table) {
-  ht_item_t *active;
 
-  if (table != NULL){
-    for(int i = 0; i < HT_SIZE; i++){
-      if(((*table)[i]) != NULL){
-        active = (*table)[i];
-        free(active);
-        (*table)[i] = NULL;
-      }
-    } 
-  } else {
+void ht_delete_all(ht_table_t *table) {
+    ht_item_t *active;
+
+    if (table != NULL){
+        for(int i = 0; i < HT_SIZE; i++){
+            if(((*table)[i]) != NULL){
+                active = (*table)[i];
+                free(active);
+                (*table)[i] = NULL;
+            }
+        }
+    } else {
       return;
-  }
+    }
 }
--- src/symtable.h
+++ src/symtable.h
@@ -1,6 +1,5 @@
 /*
  * Created by Marek Spirka and Jasmina Csalova on 17.11.2022
- *
  * Inspired by the second project from the IAL 2022 course
  */
 
@@ -14,16 +13,25 @@
 
 extern int HT_SIZE;
 
+/**
+ * Struct for argument
+ */
 typedef struct arg{
     Token arg;
     Token type;
 } arg;
 
+/**
+ * @brief Struct for argumentList
+ */
 typedef struct argList{
     arg* list;
     int len;
 } argList;
 
+/**
+ * @brief Struct for element
+ */
 typedef struct element{
     Token name;
     argList* argslist;
@@ -32,20 +40,48 @@ typedef struct element{
     bool nullRet;
 } element;
 
-
+/**
+ * @brief Struct for item in table
+ */
 typedef struct ht_item {
   char key;
   element e;
 } ht_item_t;
 
-
 typedef ht_item_t *ht_table_t[MAX_HT_SIZE];
-
+/**
+ * @brief Function for creating a hash key for the element that we store in the table
+ * @param *key - pointer to the key from which we will create a hash
+ */
 int get_hash(char *key);
+/**
+ * @brief Function to create and initialize the sym table
+ * @param *table - pointer to the table
+ */
 void ht_init(ht_table_t *table);
+/**
+ * @brief Function that uses a hash key to search for an element in the table
+ * @param *table - pointer to the table
+ * @param *key - pointer to key that we want to find
+ */
 ht_item_t *ht_search(ht_table_t *table, char *key);
+/**
+ * @brief Function that stores an element in a table
+ * @param *table - pointer to the table
+ * @param *key - pointer to the key with which we save the element in the table
+ * @param *data - pointer to data that we want to insert
+ */
 void ht_insert(ht_table_t *table, char *key, element *data);
+/**
+ * @brief Function to get an element from the table
+ * @param *table - pointer to the table
+ * @param *key - pointer to the key with which we search in the table
+ */
 element *ht_get(ht_table_t *table, char *key);
+/**
+ * @brief Function to delete the whole table
+ * @param *table - pointer to the table
+ */
 void ht_delete_all(ht_table_t *table);
 
 #endif
